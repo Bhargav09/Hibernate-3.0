554f4b55af2aca5699c70e853d19870457716257
BHARGAV GANGADHARA <bhargav.gangadhara@aol.com>
2016-09-16 11:39:33 -0400

    Add files via upload

---
+++ PO2.java
@@ -0,0 +1,29 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Bhargav Gangadhara
+ */
+@Entity
+@Table(name = "`ANY`")
+public class Any {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String prop;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getProp() {
+		return prop;
+	}
+
+	public void setProp(String prop) {
+		this.prop = prop;
+	}
+}
